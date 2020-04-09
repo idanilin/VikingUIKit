7cd5170f059a4abd3c7be26709c99ae8e733c03f
idanilin <idanilin78@gmail.com>
2020-04-09 15:36:23 +0300

    Initial Commit

---
+++ VikingUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VikingUIKit
+//
+//  Created by Ivan on 09/04/2020.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
